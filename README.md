b84c3929b1336cb21c6240353509acc5692e6903
MELFI PEREZ <perezmelfi@outlook.com>
2019-07-10 12:04:34 -0700

    lab7 completed with added API functionality

--- README.md
+++ README.md
@@ -23,6 +23,8 @@ Technology: VS code, node.js
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
 07-09-2019 11:44am - Application has a fully functional server, with a GET route for the location and weather data. We deployed our application on Heroku. 
 
+07-10-2019 12:03pm - Application has a fully functional API calls, with a GET route for the location and weather data. We deployed our application on Heroku. EventBrite API functionality added.  
+
 
 
 ## Timeline
@@ -70,3 +72,57 @@ Start time: 1125
 Finish time: 1135
 
 Actual time needed to complete: 10 min
+
+## Lab7
+
+Number and name of feature: 1. Data Formatting
+
+Estimate of time needed to complete: 30mins  
+
+Start time: 9:10am  
+
+Finish time: 9:40am  
+
+Actual time needed to complete: 30mins  
+
+
+
+Number and name of feature: 2. Location API
+
+Estimate of time needed to complete: 1 hr
+
+Start time: 9:40am  
+
+Finish time: 10:35am  
+
+Actual time needed to complete: 55mins  
+
+
+
+Number and name of feature: 3. Weather API
+
+Estimate of time needed to complete: 1 hr  
+
+Start time: 10:35am  
+
+Finish time: 11:00am  
+
+Actual time needed to complete: 25mins  
+
+
+Number and name of feature: 4. EventBrite API
+
+Estimate of time needed to complete: 1hr  
+
+Start time: 11:00am  
+
+Finish time: 12:00pm  
+
+Actual time needed to complete: 1hr  
+
+
+## Credits and Collaborations
+Kevin Couture  
+Sapana Poudel  
+Melfi Perez  
+Jane Hur  
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 // Globals
 const PORT = process.env.PORT || 3009;
@@ -13,65 +14,82 @@ const app = express();
 app.use(cors());
 
 // Location Route
-app.get('/location', (request, response) => {
-  // console.log(request.query.data);
-  try {
-    const locationData = searchToLatLng(request.query.data);
-    response.send(locationData);
-  }
-  catch (e) {
-    response.status(500).send('Status 500, not functional.');
-  }
-});
+app.get('/location', searchToLatLng);
 
 // Weather Route
-app.get('/weather', (request, response) => {
-  try {
-    const weatherData = getWeatherRoute(request.query.data);
-    response.send(weatherData);
-  }
-  catch (e) {
-    response.status(500).send('Status 500, not functional.');
-  }
-});
+app.get('/weather', getWeatherRoute);
+
+//EventBrite Route
+app.get('/events',getEventRoute);
 
 // app.use('*', (request, response) => {
 //   response.send('you got to the wrong place');
 // });
 
-function Location(name, formatted, lat, lng) {
-  this.search_query = name;
-  this.formatted_query = formatted;
-  this.latitude = lat;
-  this.longitude = lng;
+//Location Constructor Start
+function Location(query, res) {
+  this.search_query = query;
+  this.formatted_query = res.body.results[0].formatted_address,
+  this.latitude = res.body.results[0].geometry.location.lat,
+  this.longitude = res.body.results[0].geometry.location.lng
 }
 
+function searchToLatLng(request, response) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
-function searchToLatLng(locationName) {
-  const geoData = require('./data/geo.json');
-  let location = new Location(
-    locationName,
-    geoData.results[0].formatted_address,
-    geoData.results[0].geometry.location.lat,
-    geoData.results[0].geometry.location.lng
-  );
-  return location;
+  return superagent.get(url)
+    .then(res =>{
+      response.send(new Location(request.query.data, res));
+    })
+    .catch(e => handleError(e));
 }
 
-function Day(forecast, time) {
-  this.forecast = forecast;
+//Weather Construtor Start
+function Day(dayObj) {
+  this.forecast = dayObj.summary;
+  let time = new Date(dayObj.time*1000).toDateString();
   this.time = time;
 }
 
-function getWeatherRoute(locationName) {
-  const weatherData = require('./data/darksky.json');
-  let daysArr = [];
-  let time;
-  for (let day of weatherData.daily.data) {
-    time = new Date(day.time*1000).toDateString();
-    daysArr.push(new Day (day.summary, time));
-  }
-  return daysArr;
+function getWeatherRoute(request, response) {
+  const url =`https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  return superagent.get(url)
+    .then(result=>{
+      const weatherSummaries = result.body.daily.data.map(day =>{
+        return new Day(day);
+      });
+      response.send(weatherSummaries);
+    })
+    .catch(e => handleError(e, response));
+}
+
+
+//EventBrite Constructor Start
+function Event (eventObj){
+  this.link = eventObj.url,
+  this.name = eventObj.name.text,
+  this.event_date = new Date(eventObj.start.local).toDateString(),
+  this.summary = eventObj.summary
+}
+
+function getEventRoute(request, response){
+  const url = `https://www.eventbriteapi.com/v3/events/search/?location.longitude=${request.query.data.longitude}&location.latitude=${request.query.data.latitude}&expand=venue&token=${process.env.EVENTBRITE_API_KEY}`;
+
+  return superagent.get(url)
+    .then(result =>{
+      console.log(result.body.events);
+      const eventSummaries = result.body.events.map(eve =>{
+        return new Event(eve)
+      });
+      response.send(eventSummaries);
+    })
+    .catch(e => handleError(e, response));
+}
+
+//Error handling
+function handleError(e, res){
+  if(res) res.status(500).send('Server Failure');
 }
 
 // Start the server.
